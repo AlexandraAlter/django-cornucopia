ee80afb194f0db738384a9b73d99a7f61bcaa8b3
AlexandraAlter <alexandra.alter@protonmail.com>
2020-09-29 23:03:29 +0100

    Corrected secret path, added staticfile keep

--- integration/dev/dev/settings.py
+++ integration/dev/dev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Secrets
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_DIR = Path(__file__).resolve().parent
+SECRET_DIR = Path(__file__).resolve().parent / 'secrets'
 
 SECRET_KEY_FILE = SECRET_DIR / 'key.txt'
 SECRET_KEY_FILE.exists() or SECRET_KEY_FILE.write_text(utils.get_random_secret_key())
