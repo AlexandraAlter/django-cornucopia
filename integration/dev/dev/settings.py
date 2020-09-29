8b8e9bdded3e95033b9a157cbdffefd9b03fe6cd
AlexandraAlter <alexandra.alter@protonmail.com>
2020-09-29 22:18:47 +0100

    Corrected a few gitignores and secret management

--- integration/dev/dev/settings.py
+++ integration/dev/dev/settings.py
@@ -22,17 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Secrets
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_DIR = Path(__file__).resolve().parent / 'secrets'
-SECRET_DIR.exists() or SECRET_DIR.mkdir()
 
-SECRET_KEY_FILE = SECRET_DIR / 'key.txt'
+SECRET_KEY_FILE = BASE_DIR / 'secrets/key.txt'
 SECRET_KEY_FILE.exists() or SECRET_KEY_FILE.write_text(utils.get_random_secret_key())
 SECRET_KEY = SECRET_KEY_FILE.read_text()
 
-SECRET_SETTINGS_FILE = SECRET_DIR / 'settings.py'
+SECRET_SETTINGS_FILE = BASE_DIR / 'secrets/settings.py'
 SECRET_SETTINGS_FILE.exists() or SECRET_SETTINGS_FILE.touch()
-SECRET_MOD_FILE = SECRET_DIR / '__init__.py'
-SECRET_MOD_FILE.exists() or SECRET_MOD_FILE.touch()
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
